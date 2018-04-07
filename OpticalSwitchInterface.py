57555ba50b5723a7f69dd169f07d4ca8c2f08a91
Alice Lai <alicelai4396@gmail.com>
2018-04-07 00:10:00 -0400

    added carriage return

--- OpticalSwitchInterface.py
+++ OpticalSwitchInterface.py
@@ -5,32 +5,36 @@ class Optical_Switch(object):
     def get_last_command(self):
         return self.command
     def scan_all(self):
-        self.command = b"1"
+        self.command = ("1" + "\n" + "\r").encode()
         self.serial.write(self.command)
     def scan_all_continuously(self):
-        self.command = b"1,1"
+        self.command = (("1,1") + "\n" + "\r").encode()
         self.serial.write(self.command)
     def scan_one(self, port):
-        self.command = (b"2,%d") % port
+        self.command = ((("2,%d") % port) + "\n" + "\r").encode()        
         self.serial.write(self.command)
     def scan_one_continously(self, port):
-        self.command = (b"2,%d,1") % port
+        self.command = ((("2,%d,1") % port) + "\n" + "\r").encode()
         self.serial.write(self.command)
     def scan_range(self, port1, port2):
-        self.command = (b"3,%d,%d") % (port1,port2)
+        self.command = (("3,%d,%d") % (port1,port2) + "\n" + "\r").encode()
         self.serial.write(self.command)
     def scan_range_continously(self, port1, port2):
-        self.command = (b"3,%d,%d,1") % (port1,port2)
+        self.command = (("3,%d,%d,1") % (port1,port2) + "\n" + "\r").encode()
         self.serial.write(self.command)
 
-#carriage return + convert everything to bytes
 import serial
+import time
+
 if __name__ == '__main__':
     arduino = serial.Serial('/dev/ttyACM0', 9600)
     myswitch = Optical_Switch(arduino)
-    myswitch.scan_one(3)
-    print(myswitch.get_last_command())
-    myswitch.scan_all_continuously()
-    print(myswitch.get_last_command())
-    myswitch.scan_range_continously(3,50)
+    myswitch.scan_all()
+    time.sleep(2)
+    myswitch.scan_range(3,50)
+    time.sleep(2)
+    myswitch.scan_one(4)
+    time.sleep(2)
+    myswitch.scan_range_continously(4,16)
+    time.sleep(2)
     print(myswitch.get_last_command())
